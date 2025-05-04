288886786ad98ada5940372d7d113b73b3017121
Perrin Njietche <perrin_wandji@pkf-researchcenter.com>
2025-05-04 21:57:02 +0100

    bugfix: add requestmapping and mockito extension

--- stargate/src/main/java/com/tech/stargate/controller/AuthController.java
+++ stargate/src/main/java/com/tech/stargate/controller/AuthController.java
@@ -3,10 +3,12 @@ package com.tech.stargate.controller;
 import com.tech.stargate.model.UserInfo;
 import com.tech.stargate.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/api/v1")
+@RestController
+@RequestMapping(("/api/v1"))
 @RequiredArgsConstructor
 public class AuthController {
 
--- stargate/src/test/java/com/tech/stargate/AuthServiceTest.java
+++ stargate/src/test/java/com/tech/stargate/AuthServiceTest.java
@@ -3,9 +3,11 @@ package com.tech.stargate;
 import com.tech.stargate.service.AuthServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
 
   @InjectMocks AuthServiceImpl authService;
